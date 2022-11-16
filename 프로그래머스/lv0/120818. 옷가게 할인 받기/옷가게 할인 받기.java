e69b9381f5c4158d6a7f77fe1bf6c4eea946d874
WannaDeveloIt <cdaosldk@naver.com>
2022-11-16 21:32:23 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int price) {
         if(price >= 100000 && price <300000){
-            return (int)(price *(1 - 0.05));
+            return (int)(price *(1 - 0.05F));
         } else if (price >= 300000 && price < 500000) {
-            return (int)(price *(1 - 0.1));
+            return (int)(price *(1 - 0.1F));
         } else if (price >= 500000 && price <= 1000000) {
-            return (int)(price *(1 - 0.2));
+            return (int)(price *(1 - 0.2F));
         }else{
             return price;
         }
