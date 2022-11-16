10cd7b07482577824dee5b081f3d67c25d494480
WannaDeveloIt <cdaosldk@naver.com>
2022-11-16 14:55:23 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,5 +1,4 @@
 def solution(price):
-    price : int
     answer = 0
     
     if 300000 > price >= 100000:
@@ -10,5 +9,4 @@ def solution(price):
         answer = price * 0.8
     else:
         answer = price
-    
     return int(answer)
