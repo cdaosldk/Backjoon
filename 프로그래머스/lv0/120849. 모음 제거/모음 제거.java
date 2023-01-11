2e87e7bc07ddf67bfc1ad3ef2abdd1d7813c8551
wannaDevelopIt <cdaosldk@naver.com>
2023-01-11 12:14:57 +0900

    [level 0] Title: 모음 제거, Time: 2.27 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+
+        List<String> myStringList = new ArrayList<>();
+
+        int array = my_string.length();
+
+        for (int i = 0; i < array; i++) {
+            myStringList.add(i, String.valueOf(my_string.charAt(i)));
+        }
+
+        myStringList = myStringList.stream().filter(s -> s.matches("[^aeiouAEIOU]$")).collect(Collectors.toList()); //.toList()와 차이??
+
+        return String.join("", myStringList);
+    }
+}
