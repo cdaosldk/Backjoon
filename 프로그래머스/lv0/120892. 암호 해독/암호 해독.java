4f4cbfab817c46e9d37480c9d116aef194db7d3d
wannaDevelopIt <cdaosldk@naver.com>
2023-01-13 09:58:35 +0900

    [level 0] Title: 암호 해독, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(String cipher, int code) {
 
-        StringBuilder stringBuffer = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
 
         int array = cipher.length();
 
         for(int i = (code-1); i < array; i += code) {
-            stringBuffer.append(cipher.charAt(i));
+            stringBuilder.append(cipher.charAt(i));
         }
 
-        return stringBuffer.toString();
+        return stringBuilder.toString();
     }
 }
