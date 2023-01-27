0300994bc8489acb6e557baf4ab808fafec7f41f
wannaDevelopIt <cdaosldk@naver.com>
2023-01-27 11:52:11 +0900

    [level 0] Title: 약수 구하기, Time: 2.43 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 1; i <= n; i++) {
+
+            if(n % i == 0) {
+                list.add(i);
+            }
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
