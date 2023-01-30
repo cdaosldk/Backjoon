4959b5daa4d731e66e226dd34b9bac675c39efe8
wannaDevelopIt <cdaosldk@naver.com>
2023-01-30 10:40:31 +0900

    [level 0] Title: 숫자 찾기, Time: 2.59 ms, Memory: 66.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,17 @@
+import java.util.stream.Stream;
+
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        int[] array = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+
+        for(int i = 0; i < array.length; i++) {
+
+            if (array[i] == k) {
+                answer = i + 1;
+                break;
+            } else answer = -1;
+        }
+        return answer;
+    }
+}
