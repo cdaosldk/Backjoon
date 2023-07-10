664a7800bc4ddbef1cef535d54a00680b33164ba
wannaDevelopIt <cdaosldk@naver.com>
2023-07-10 23:06:27 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) == 'A') {
+                sb.append('B');
+            } else sb.append('A');
+        }
+
+        String s = sb.toString();
+
+        if (s.contains(pat)) {
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
