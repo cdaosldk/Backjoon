70c9362d5cfbfe3d0d202b2b8e9541f5bd4658df
wannaDevelopIt <cdaosldk@naver.com>
2023-12-22 02:07:40 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 3.15 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+
+        int[] answer = new int[queries.length];
+        int count = 0;
+
+        for (int i = 0; i < queries.length; i++) {
+            int tmp = 1000000;
+            count = 0;
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                if (arr[j] > queries[i][2] && tmp >= arr[j]) {
+                    tmp = arr[j];
+                    count++;
+                } else if (j == queries[i][1] && count == 0) {
+                    tmp = -1;
+                }
+            }
+            answer[i] = tmp;
+        }
+
+        return answer;
+    }
+}
